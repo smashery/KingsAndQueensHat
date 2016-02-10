85a4f2ff718c577a5aa12874cf6e5152b5bd910c
Ashley Donaldson <smashery@gmail.com>
2016-02-10 23:14:45 +1100

    Missed files

---
+++ KingsAndQueensHat/Model/SkillLevel.cs
@@ -0,0 +1,64 @@
+﻿using KingsAndQueensHat.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace KingsAndQueensHat.Model
+{
+    [XmlType(TypeName = "SkillLookup")]
+    public class SkillLevel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        private int _value;
+        [XmlElement("Skill")]
+        public int Value 
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (value != _value)
+                {
+                    _value = value;
+                    OnPropertyChanged("Skill");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
